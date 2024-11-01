9026db629b7416df612cec46da10b3acd240b039
zsusinskis <zsusinskis@gmail.com>
2024-11-01 23:44:44 +0200

    Google Places API requests/response

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,13 @@
+import org.gradle.api.JavaVersion
+import java.io.FileInputStream
+import java.util.Properties
+
+val properties = Properties().apply {
+    load(FileInputStream(rootProject.file("local.properties")))
+}
+
+val apiKey = properties.getProperty("API_KEY");
+
 plugins {
     alias(libs.plugins.android.application)
 }
@@ -14,6 +24,11 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        buildConfigField("String", "API_KEY", "\"$apiKey\"")
+    }
+
+    buildFeatures {
+        buildConfig = true
     }
 
     buildTypes {
@@ -29,6 +44,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
 }
 
 dependencies {
@@ -37,6 +53,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.play.services.location)
+    implementation(libs.volley)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
